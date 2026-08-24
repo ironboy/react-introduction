744a651277a35ba13699896371bfc434e14f9b76
Ironboy <ironboy@users.noreply.github.com>
2026-08-24 15:26:56 +0200

    moving state to parent component App - one state per counter -and sending setter functions as props to the button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import CounterButton from "./CounterButton";
 
 export default function App() {
@@ -6,11 +7,21 @@ export default function App() {
   // When we change a state the affected view(s) updates automatically
   // No manual DOM manipulation is needed (or should be done)
 
+  // Note: RULES OF HOOKS IS SUPER IMPORTANT IN React.
+  // You MAY NOT call a hook after an early return or inside a condition
+
+  // a state variable and its setter is returned from useState
+  // you give useState the initial value as a parameter
+
+  const [counter1, setCounter1] = useState(10);
+  const [counter2, setCounter2] = useState(100);
+
   return <>
     <h3>Hello world!</h3>
     <p>This comes from the App component in my React application.</p>
     {/* A component can mount (include) another component: */}
-    <CounterButton startValue={10} max={20} />
-    <CounterButton startValue={100} max={110} />
+    <CounterButton counter={counter1} setCounter={setCounter1} max={20} />
+    <CounterButton counter={counter2} setCounter={setCounter2} max={110} />
+    <p>The sum of our two counters is {counter1 + counter2}.</p>
   </>;
 }
--- src/CounterButton.jsx
+++ src/CounterButton.jsx
@@ -1,18 +1,9 @@
-import { useState } from 'react';
-
 // props is an object with the properties/"attributes"
 // you send to the component when you mount it in jsx
 export default function CounterButton(props) {
 
   // destructure the props into separate variables/constant
-  const { startValue, max } = props;
-
-  // Note: RULES OF HOOKS IS SUPER IMPORTANT IN React.
-  // You MAY NOT call a hook after an early return or inside a condition
-
-  // a state variable and its setter is returned from useState
-  // you give useState the initial value as a parameter
-  const [counter, setCounter] = useState(startValue);
+  const { counter, setCounter, max } = props;
 
   return <button onClick={() => counter < max && setCounter(counter + 1)}>
     {counter}
