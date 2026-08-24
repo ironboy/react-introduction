7280c30a9a27e4bd56e818a14dad896148450276
Ironboy <ironboy@users.noreply.github.com>
2026-08-24 14:11:19 +0200

    comment about react, state and views

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
 export default function App() {
+
+  // React is basically a state -> view handling library/framework
+  // When we change a state the affected view(s) updates automatically
+  // No manual DOM manipulation is needed (or should be done)
+
   return <div>
     <h3>Hello world!</h3>
     <p>This comes from the App component in my React application.</p>
